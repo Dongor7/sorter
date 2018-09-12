e910a6cd720447d346456366ed25fa33163cb631
Denis Sidorenko <dongor7@gmail.com>
2018-09-12 11:12:45 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.store = [];
+    this.compataror = null;
   }
 
   add(element) {
-    // your implementation
+    this.store.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.store[index];
   }
 
   get length() {
-    // your implementation
+    return this.store.length;
   }
 
   toArray() {
-    // your implementation
+    return this.store;
   }
 
   sort(indices) {
-    // your implementation
+
+    let sortedIndices = indices;
+    sortedIndices.sort();
+
+    let arr = [];
+
+    sortedIndices.forEach((i) => {
+      arr.push(this.store[i]);
+    });
+
+    if (this.compataror) {
+      arr.sort(this.compataror);
+    } else {
+      arr.sort((a, b) => a - b);
+    }
+
+    indices.forEach((item, i) => {
+      this.store.splice(indices[i], 1, arr[i]);
+    })
+
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compataror = compareFunction;
   }
 }
 
